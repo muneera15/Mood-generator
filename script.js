894f1e77f6a763062f820c537ea617fca1d969ff
muneera <muneerashaik03@gmail.com>
2025-04-26 00:57:27 +0530

    css updated

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const moods = {
     happy:{
         quote:"Happiness is not by chance, but by choice.",
         image:"assets/images/happy.jpg",
-        music:`<iframe width="560" height="315" src="https://www.youtube.com/embed/ZbZSe6N_BXs?si=eiwek_1Z2DKUBP-7" title="YouTube video player" frameborder="1px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen ></iframe>`
+        music: `<iframe width="560" height="315" src="https://www.youtube.com/embed/MOWDb2TBYDg?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     sad:{
         quote:"Your present circumstances don’t determine where you go; they merely determine where you start.",
-        image:"assets/sad.jpg",
-        music:`<iframe width="560" height="315" src="https://www.youtube.com/embed/W0DM5lcj6mw?si=fXaw6tH_ZfOCkOaz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        image:"assets/images/sad.jpg",
+        music: `<iframe width="560" height="315" src="https://www.youtube.com/embed/1AqdWNS6Mv0?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
-    lazy:{
-        quote:"Success doesn’t come from what you do occasionally, it comes from what you do consistently.",
-        image:"assets/images/lazy.jpg",
-        music:`<iframe width="560" height="315" src="https://www.youtube.com/embed/h3h035Eyz5A?si=CT-ntxgdE6SQQYl0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
-    },
+    lazy: {
+        quote: "Resting is not laziness, it’s medicine!",
+        music: `<iframe width="240" height="135" src="https://www.youtube.com/embed/vcxQMI55As0?autoplay=1&mute=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="border-radius: 12px !important;"></iframe>`,
+        image: "assets/images/lazy.jpg"
+      },
     energetic:{
         quote:"The only way to do great work is to love what you do.",
         image:"assets/images/energetic.jpg",
-        music:`<iframe width="560" height="315" src="https://www.youtube.com/embed/IL_QlRXzhsQ?si=U0eS-6S3fNAeCDG5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        music:`<iframe width="560" height="315" src="https://www.youtube.com/embed/IL_QlRXzhsQ?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     }
 };
 const moodSelector = document.getElementById("mood-selector");
