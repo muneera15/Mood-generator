87a123a5a2198806412ad9cde9a631afc358cc34
muneera <muneerashaik03@gmail.com>
2025-04-26 16:25:12 +0530

    added some functionality in css

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const moods = {
         music: `<iframe width="560" height="315" src="https://www.youtube.com/embed/MOWDb2TBYDg?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     sad:{
-        quote:"Your present circumstances don’t determine where you go; they merely determine where you start.",
+        quote:"Your present circumstances don’t determine where you go;they merely determine where you start.",
         image:"assets/images/sad.jpg",
         music: `<iframe width="560" height="315" src="https://www.youtube.com/embed/1AqdWNS6Mv0?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     lazy: {
-        quote: "Resting is not laziness, it’s medicine!",
+        quote: "Success doesn’t come from what you do occasionally, it comes from what you do consistently.",
         music: `<iframe width="240" height="135" src="https://www.youtube.com/embed/vcxQMI55As0?autoplay=1&mute=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="border-radius: 12px !important;"></iframe>`,
         image: "assets/images/lazy.jpg"
       },
@@ -27,6 +27,9 @@ const musicDisplay = document.getElementById("music");
 moodSelector.addEventListener('change',()=>{
     const selectedMood =moodSelector.value;
     document.body.className="";
+    quoteDisplay.textContent="";
+    imageDisplay.src="";
+    musicDisplay.innerHTML="";
 
 if(selectedMood && moods[selectedMood]){
     const moodData = moods[selectedMood];
@@ -37,7 +40,7 @@ if(selectedMood && moods[selectedMood]){
 }
 else{
     quoteDisplay.textContent = "";
-    imageDisplay.src = "";
+    imageDisplay = "";
     musicDisplay.innerHTML = "";
 }
 });
